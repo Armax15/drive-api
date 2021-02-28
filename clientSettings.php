01c06425554a1e5cf90ad20200f87cdde4eec217
Artem Muryumin <muryumin15@gmail.com>
2021-02-28 20:46:23 +0300

    requirement php >= 7.4 moved credentials to separate dir and added one to .gitignore

--- clientSettings.php
+++ clientSettings.php
@@ -1,7 +1,8 @@
 <?php
 
-
 use root\ClientSettings;
+
+define('CREDENTIALS_DIR', __DIR__);
 
 return [
     ClientSettings::FIELD_APP_NAME    => 'test',
@@ -9,6 +10,6 @@ return [
         Google_Service_Drive::DRIVE
     ],
     ClientSettings::FIELD_ACCESS_TYPE => 'offline',
-    ClientSettings::FIELD_AUTH_DATA   => 'credentials-googleDrive.json',
-    ClientSettings::FIELD_TOKEN_PATH  => 'token.json'
+    ClientSettings::FIELD_AUTH_DATA   => CREDENTIALS_DIR . '/credentials-googleDrive.json',
+    ClientSettings::FIELD_TOKEN_PATH  => CREDENTIALS_DIR . '/token.json'
 ];
